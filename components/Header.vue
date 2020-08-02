d7be98a9ad69ef19d80a1d9d708b098856b65da2
sou <sasaki.sou@samurai-adways.net>
2020-08-02 13:01:01 +0900

    header追加

---
+++ components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Demo</b-navbar-brand>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item to="/">ルートリンク</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
