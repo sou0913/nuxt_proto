257a619c89c5106ef3ae588ea34522d9a56554a2
sou <sasaki.sou@samurai-adways.net>
2020-08-02 15:38:54 +0900

    リンク追加

--- pages/employees.vue
+++ pages/employees.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <b-table striped hover :items="employees"></b-table>
+    <b-table striped hover :items="employees">
+      <template v-slot:cell(name)="data">
+        <b-link to="/">{{ data.value }}</b-link>
+      </template>
+    </b-table>
   </div>
 </template>
 
@@ -9,10 +13,10 @@ export default {
   data() {
     return {
       employees: []
-    }
+    };
   },
   async fetch() {
-    this.employees = await this.$axios.$get('http://localhost:3000/employees')
-  },
+    this.employees = await this.$axios.$get("http://localhost:3000/employees");
+  }
 };
 </script>
