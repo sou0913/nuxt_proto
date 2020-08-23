6c61da91c7f8d3ce50ecd6665c000880c337878a
sou <sasaki.sou@samurai-adways.net>
2020-08-23 20:26:15 +0900

    問題から一致した部分を赤にし、成否判定を出す

--- pages/problem/_id.vue
+++ pages/problem/_id.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <p>問題:aをbに入れ替えてください</p>
+    <p>問題: {{ title }}</p>
     <b-form-input v-model="regex" :state="regexState" placeholder="正規表現"></b-form-input>
     <!-- <b-form-input v-model="replacement" placeholder="置換後文字列"></b-form-input> -->
-    <p>{{ target }}</p>
+    <p v-html="coloredText"></p>
     <div v-show="regexState">
     <p>正解!</p>
     <nuxt-link to="/regex">一覧へ戻る</nuxt-link>
@@ -18,16 +18,22 @@ export default {
     return {
       regex: '',
       // replacement: '',
-      target: ''
+      title: '',
+      target: 'abc',
+      text: 'abcdefg',
     };
   },
   computed: {
     regexState() {
-      return this.target == this.target.match(this.regex) ? true : false
+      return this.target == this.text.match(this.regex) ? true : false
+    },
+    coloredText() {
+      return this.text.replace(new RegExp(`([\s\S]*)(${this.regex})([\s\S]*)`), "<font color='red'>$2</font>")
     }
   },
   async fetch() {
     const response = await this.$axios.$get(`/problems/${this.$route.params.id}`)
+    this.title = response['title']
     this.target = response['target']
   }
 };
