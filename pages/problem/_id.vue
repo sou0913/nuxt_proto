a6c9b6b68dbbffc3ca11963b422543db31f4c253
sou <sasaki.sou@samurai-adways.net>
2020-08-23 20:43:01 +0900

    カラム追加

--- pages/problem/_id.vue
+++ pages/problem/_id.vue
@@ -3,7 +3,7 @@
     <p>問題: {{ title }}</p>
     <b-form-input v-model="regex" :state="regexState" placeholder="正規表現"></b-form-input>
     <!-- <b-form-input v-model="replacement" placeholder="置換後文字列"></b-form-input> -->
-    <p v-html="coloredText"></p>
+    <p v-html="coloredStatement"></p>
     <div v-show="regexState">
     <p>正解!</p>
     <nuxt-link to="/regex">一覧へ戻る</nuxt-link>
@@ -19,22 +19,23 @@ export default {
       regex: '',
       // replacement: '',
       title: '',
-      target: 'abc',
-      text: 'abcdefg',
+      target: '',
+      statement: '',
     };
   },
   computed: {
     regexState() {
-      return this.target == this.text.match(this.regex) ? true : false
+      return this.target == this.statement.match(this.regex) ? true : false
     },
-    coloredText() {
-      return this.text.replace(new RegExp(`([\s\S]*)(${this.regex})([\s\S]*)`), "<font color='red'>$2</font>")
+    coloredStatement() {
+      return this.statement.replace(new RegExp(`([\s\S]*)(${this.regex})([\s\S]*)`), "<font color='red'>$2</font>")
     }
   },
   async fetch() {
     const response = await this.$axios.$get(`/problems/${this.$route.params.id}`)
     this.title = response['title']
     this.target = response['target']
+    this.statement = response['statement']
   }
 };
 </script>
