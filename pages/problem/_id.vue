4dc81b16b5259aacf2d9ed6b8d99825e978bfaa0
sou <sasaki.sou@samurai-adways.net>
2020-08-23 14:35:22 +0900

    マッチしているか否かの判定初歩

--- pages/problem/_id.vue
+++ pages/problem/_id.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <p>問題:aをbに入れ替えてください</p>
-    <b-form-input v-model="regex" placeholder="正規表現"></b-form-input>
+    <b-form-input v-model="regex" :state="regexState" placeholder="正規表現"></b-form-input>
     <b-form-input v-model="replacement" placeholder="置換後文字列"></b-form-input>
-    <p>a</p>
+    <p>{{ target }}</p>
   </div>
 </template>
 
@@ -13,7 +13,13 @@ export default {
     return {
       regex: '',
       replacement: '',
+      target: 'abc'
     };
+  },
+  computed: {
+    regexState() {
+      return this.target.match(this.regex) ? true : false
+    }
   }
   // async fetch() {
   //   this.form= await this.$axios.$get(`http://localhost:3000/employees/${this.$route.params.id}`)
