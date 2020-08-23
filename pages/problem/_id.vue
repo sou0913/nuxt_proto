1a317207f34850b4151b2ad0b807125e64a361d7
sou <sasaki.sou@samurai-adways.net>
2020-08-23 14:06:29 +0900

    問題ページの基本ビュー作成

--- pages/problem/_id.vue
+++ pages/problem/_id.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <p>問題:aをbに入れ替えてください</p>
+    <b-form-input v-model="regex" placeholder="正規表現"></b-form-input>
+    <b-form-input v-model="replacement" placeholder="置換後文字列"></b-form-input>
+    <p>a</p>
   </div>
 </template>
 
@@ -7,8 +11,10 @@
 export default {
   data() {
     return {
+      regex: '',
+      replacement: '',
     };
-  },
+  }
   // async fetch() {
   //   this.form= await this.$axios.$get(`http://localhost:3000/employees/${this.$route.params.id}`)
   // }
