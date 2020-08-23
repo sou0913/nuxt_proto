ab4253b8127fe5ed93285537ce4c3d8386d20e6c
sou <sasaki.sou@samurai-adways.net>
2020-08-23 20:57:32 +0900

    正規表現エラーだけ握り潰す

--- pages/problem/_id.vue
+++ pages/problem/_id.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
     <p>問題: {{ title }}</p>
-    <b-form-input v-model="regex" :state="regexState" placeholder="正規表現"></b-form-input>
-    <!-- <b-form-input v-model="replacement" placeholder="置換後文字列"></b-form-input> -->
+    <p>
+      下の文字列から{{
+        target
+      }}にマッチする正規表現を入力してください（/は不要です）
+    </p>
     <p v-html="coloredStatement"></p>
+    <b-form-input
+      v-model="regex"
+      :state="regexState"
+      placeholder="正規表現"
+    ></b-form-input>
+    <!-- <b-form-input v-model="replacement" placeholder="置換後文字列"></b-form-input> -->
     <div v-show="regexState">
-    <p>正解!</p>
-    <nuxt-link to="/regex">一覧へ戻る</nuxt-link>
-    <nuxt-link :to="'/problem/' + (parseInt(this.$route.params.id) + 1)">次の問題へ</nuxt-link>
+      <p>正解!</p>
+      <nuxt-link to="/regex">一覧へ戻る</nuxt-link>
+      <nuxt-link :to="'/problem/' + (parseInt(this.$route.params.id) + 1)"
+        >次の問題へ</nuxt-link
+      >
     </div>
   </div>
 </template>
@@ -16,26 +27,45 @@
 export default {
   data() {
     return {
-      regex: '',
+      regex: "",
       // replacement: '',
-      title: '',
-      target: '',
-      statement: '',
+      title: "",
+      target: "",
+      statement: ""
     };
   },
   computed: {
     regexState() {
-      return this.target == this.statement.match(this.regex) ? true : false
+      try {
+        return this.target == this.statement.match(this.regex) ? true : false;
+      } catch (e) {
+        if (e instanceof SyntaxError) {
+        } else {
+          logMyErrors(e);
+        }
+      }
     },
     coloredStatement() {
-      return this.statement.replace(new RegExp(`([\s\S]*)(${this.regex})([\s\S]*)`), "<font color='red'>$2</font>")
+      try {
+        return this.statement.replace(
+          new RegExp(`([\s\S]*)(${this.regex})([\s\S]*)`),
+          "<font color='red'>$2</font>"
+        );
+      } catch (e) {
+        if (e instanceof SyntaxError) {
+        } else {
+          logMyErrors(e);
+        }
+      }
     }
   },
   async fetch() {
-    const response = await this.$axios.$get(`/problems/${this.$route.params.id}`)
-    this.title = response['title']
-    this.target = response['target']
-    this.statement = response['statement']
+    const response = await this.$axios.$get(
+      `/problems/${this.$route.params.id}`
+    );
+    this.title = response["title"];
+    this.target = response["target"];
+    this.statement = response["statement"];
   }
 };
 </script>
