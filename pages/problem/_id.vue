36d05761a08fba23ea437a97e38348f63c73d3df
sou <sasaki.sou@samurai-adways.net>
2020-08-27 16:07:21 +0900

    replaceの見た目追加

--- pages/problem/_id.vue
+++ pages/problem/_id.vue
@@ -12,7 +12,12 @@
       :state="regexState"
       placeholder="正規表現"
     ></b-form-input>
-    <!-- <b-form-input v-model="replacement" placeholder="置換後文字列"></b-form-input> -->
+    <b-form-input
+      v-if="after_replace_answer"
+      v-model="replacement"
+      :state="replacementState"
+      placeholder="置換後文字列"
+    ></b-form-input>
     <div v-show="regexState">
       <p>正解!</p>
       <nuxt-link to="/regex">一覧へ戻る</nuxt-link>
@@ -24,17 +29,34 @@
 export default {
   data() {
     return {
+      type: "",
       regex: "",
-      // replacement: '',
+      replacement: "",
       title: "",
       target: "",
-      statement: ""
+      statement: "",
+      senario: "",
+      after_replace_answer: ""
     };
   },
   computed: {
     regexState() {
       try {
-        return this.target == this.statement.match(this.regex) ? true : false;
+        return this.target == this.statement.match(this.regex);
+      } catch (e) {
+        if (e instanceof SyntaxError) {
+        } else {
+          logMyErrors(e);
+        }
+      }
+    },
+    replacementState() {
+      try {
+        const after_replace = this.statement.replace(
+          this.regex,
+          this.replacement
+        );
+        return this.after_replace_answer == after_replace;
       } catch (e) {
         if (e instanceof SyntaxError) {
         } else {
@@ -54,15 +76,18 @@ export default {
           logMyErrors(e);
         }
       }
-    },
+    }
   },
   async fetch() {
     const response = await this.$axios.$get(
       `/problems/${this.$route.params.id}`
     );
+    this.type = response["type"];
     this.title = response["title"];
     this.target = response["target"];
     this.statement = response["statement"];
+    this.senario = response["senario"]
+    this.after_replace_answer = response["after_replace_answer"]
   }
 };
 </script>
