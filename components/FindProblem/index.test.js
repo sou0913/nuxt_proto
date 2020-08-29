eae5242efc2b3c50ee2d01c1f7ee9b5d59d0643e
sou <sasaki.sou@samurai-adways.net>
2020-08-29 10:45:05 +0900

    hosting先変更

---
+++ components/FindProblem/index.test.js
@@ -0,0 +1,11 @@
+import { shallowMount } from '@vue/test-utils'
+import FindProblem from '@/components/FindProblem'
+
+describe('FindProblem', () => {
+  const props = {
+      target: 'abc',
+      statement: 'abcdef',
+  }
+  it('target(マッチ対象)が表示されているか確認', () => {})
+  it ('statement(全体文)が表示されているか確認', () => {})
+})
